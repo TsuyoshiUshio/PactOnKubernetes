5f78ab73e92a96a8de2423eaabc34213ef4ee2ba
TsuyoshiUshio <ushio@simplearchitect.com>
2017-05-11 21:08:00 -0700

    Adding Consumer Pact Test for local file

---
+++ consumer/recommend_service_test.go
@@ -0,0 +1,100 @@
+package consumer
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"os"
+	"strings"
+	"testing"
+
+	"net/http"
+
+	"net/http/httptest"
+
+	"github.com/pact-foundation/pact-go/dsl"
+)
+
+// test data
+var dir, _ = os.Getwd()
+var pactDir = fmt.Sprintf("%s/../pacts", dir)
+var logDir = fmt.Sprintf("%s/log", dir)
+var pact dsl.Pact
+var searchRequest = ` { "keyword":"protein"}`
+var form url.Values
+var rr http.ResponseWriter
+var req *http.Request
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	pact.WritePact()
+	pact.Teardown()
+	os.Exit(code)
+}
+
+func setup() {
+	pact = createPact()
+
+	form = url.Values{}
+	form.Add("keyword", "protein")
+
+	req, _ = http.NewRequest("POST", "/search", strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.PostForm = form
+
+	rr = httptest.NewRecorder()
+}
+
+func createPact() dsl.Pact {
+	pactDaemonPort := 6666
+	return dsl.Pact{
+		Port:     pactDaemonPort,
+		Consumer: "recommendation",
+		Provider: "product",
+		LogDir:   logDir,
+		PactDir:  pactDir,
+	}
+}
+
+func TestPactConsumerSearchHandler_ProductExists(t *testing.T) {
+	var testProteinExists = func() error {
+		client := Client{
+			Host: fmt.Sprintf("http://localhost:%d", pact.Server.Port),
+		}
+		client.searchHandler(rr, req)
+		if client.Product == nil {
+			return errors.New("Expected product not to be nil")
+		}
+		return nil
+	}
+	pact.
+		AddInteraction().
+		Given("Product protein exists").
+		UponReceiving("A request to search with keyword 'protein'").
+		WithRequest(dsl.Request{
+			Method: "POST",
+			Path:   "/products/search",
+			Body:   searchRequest,
+		}).
+		WillRespondWith(dsl.Response{
+			Status: 200,
+			Headers: map[string]string{
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			Body: `
+				{
+					"product": {
+						"Id":    1,
+						"Name":  "Protein",
+						"Price": 40
+					}
+				}
+				`,
+		})
+
+	err := pact.Verify(testProteinExists)
+	if err != nil {
+		t.Fatalf("Error on Verify: %v", err)
+	}
+}
